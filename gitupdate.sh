5c1780eb5de9576504476b233046814d495afb81
Jerry Workman <jerry.workman@gmail.com>
2019-02-17 21:04:44 -0500

    edit gitignore

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-echo "do a git add . first"
-#echo jerry.workman@gmail.com
-# echo UrUTg2FLZlJqWIdnR2A3
-# cd /home/homeassistant/.homeassistant
-#source /srv/homeassistant/homeassistant_venv/bin/activate
+cd /home/homeassistant/.homeassistant
 source /srv/homeassistant/bin/activate
 hass --script check_config
 
 git add .
 git status
+# CHANGE_MSG="Minor Update"
 echo -n "Enter the Description for the Change: " [Minor Update]
 read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
