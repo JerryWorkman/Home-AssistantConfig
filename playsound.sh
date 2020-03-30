50fcd5440476a465fb5ffff48b2ec3d05a68fa26
Jerry Workman <jerry.workman@gmail.com>
2020-03-30 18:23:24 -0400

    speaker

--- playsound.sh
+++ playsound.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 #set tp 100% volume
-#amixer set PCM -- -0
-amixer set PCM -- 50%
-aplay "/usr/share/scratch/Media/Sounds/Instruments/Suspense.wav"
+#amixer set PCM -- 100%
+#aplay "/home/homeassistant/.homeassistant/doorbell-2.mp3"
+mpg123 -g 100 "/home/homeassistant/.homeassistant/doorbell-2.mp3"
+#mplayer -ao pulse "/home/homeassistant/.homeassistant/doorbell-2.mp3"
--- upgrade.sh
+++ upgrade.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-cd /home/homeassistant/.homeassistant
-source /srv/homeassistant/bin/activate
+#cd /home/homeassistant/.homeassistant
+#source /srv/homeassistant/bin/activate
 
-pip install --upgrade homeassistant
+#pip install --upgrade homeassistant
 
-#sudo docker pull homeassistant/home-assistant:latest
-#sudo docker container ls 
-#sudo docker restart 8595d093558e
+sudo docker pull homeassistant/home-assistant:latest
+sudo docker container ls 
+sudo docker restart e24aabc67b1f
 
-hass --script check_config
+#hass --script check_config
+docker run -it --rm -v /home/homeassistant/.homeassistant:/config -v /etc/localtime:/etc/localtime:ro homeassistant/home-assistant:stable hass -c /config --script check_config
 
-sudo systemctl restart home-assistant@homeassistant
+#sudo systemctl restart home-assistant@homeassistant
 
-exit
+#exit
 
